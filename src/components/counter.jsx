af4901cc25e2a5ad4912e878204dab4406fa83de
yigitkesers <165410558+yigitkesers@users.noreply.github.com>
2024-05-19 17:36:24 +0300

    form.jsx düzenlendi

---
+++ src/components/counter.jsx
@@ -0,0 +1,28 @@
+// import React, { useState } from "react";
+
+// export default function Counter() {
+//   const [count, setCount] = useState(0);
+
+//   const azalt = () => {
+//     setCount(count - 1);
+//   };
+
+//   const arttır = () => {
+//     setCount(count + 1);
+//   };
+
+//   return (
+//     <div
+//       style={{
+//         display: "flex",
+//         alignItems: "center",
+//         justifyContent: "center",
+//         gap: "20px",
+//       }}
+//     >
+//       <button onClick={azalt}>-</button>
+//       <span>{count}</span>
+//       <button onClick={arttır}>+</button>
+//     </div>
+//   );
+// }
--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -1,6 +1,14 @@
 import logo from "../assets/home-banner.png";
 import "../style/homepage.css";
+import { useHistory } from "react-router-dom";
+
+
 const HomePage = () => {
+    let history = useHistory();
+  
+    function handleSubmit() {
+      history.push("/Form");
+    }
     return (
         <div className="homepage-content">
             <img className="homepage-content-img" src={logo} alt={logo}></img>
@@ -9,7 +17,7 @@ const HomePage = () => {
             PİZZA, DOYURUR
             </h1>
             <br />
-            <button className="homepage-content-btn">ACIKTIM</button>
+            <button className="homepage-content-btn" onClick={handleSubmit}>ACIKTIM</button>
         </div>
         
     )
